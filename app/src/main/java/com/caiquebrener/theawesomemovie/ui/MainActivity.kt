7d1761c3ed326ef6ade5d2a419d48b52e332d587
C-Brener <brenercaique0806@gmail.com>
2022-10-12 00:24:36 -0300

    Criando e configurando toolbar

--- app/src/main/java/com/caiquebrener/theawesomemovie/ui/MainActivity.kt
+++ app/src/main/java/com/caiquebrener/theawesomemovie/ui/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.caiquebrener.theawesomemovie.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.caiquebrener.theawesomemovie.R
+import com.caiquebrener.theawesomemovie.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navDrawer: NavigationView
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        drawerLayout = binding.root
+        navDrawer = binding.navigationView
+
+        setupNavigationComponent()
+        setupToolBar()
+    }
+
+    private fun setupNavigationComponent() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+    private fun setupToolBar() {
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.listMoviesFragment, R.id.moviesDetailsFragment),drawerLayout)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
