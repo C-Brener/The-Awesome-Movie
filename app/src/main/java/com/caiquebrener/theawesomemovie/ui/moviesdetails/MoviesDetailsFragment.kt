9ca6fe6ae381153d115e9f37a0603d2cb6143c3a
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2023-10-23 17:01:10 -0300

    Adicionando DataBinding na tela de detalhes

--- app/src/main/java/com/caiquebrener/theawesomemovie/ui/moviesdetails/MoviesDetailsFragment.kt
+++ app/src/main/java/com/caiquebrener/theawesomemovie/ui/moviesdetails/MoviesDetailsFragment.kt
@@ -5,29 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.caiquebrener.theawesomemovie.R
 import com.caiquebrener.theawesomemovie.databinding.FragmentMoviesDetailsBinding
 import com.caiquebrener.theawesomemovie.ui.viewmodel.MoviesViewModel
 
 class MoviesDetailsFragment : Fragment() {
-    private var _binding:FragmentMoviesDetailsBinding? = null
-    val binding get() = _binding!!
-    val viewMovies: MoviesViewModel by activityViewModels()
+    private lateinit var binding: FragmentMoviesDetailsBinding
+    private val viewModel
+    : MoviesViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMoviesDetailsBinding.inflate(inflater,container,false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_movies_details,
+            container,
+            false
+        )
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.moviesDetails = viewModel
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.titleMovieDetails.text = viewMovies.name.value?.title
-        binding.contentHqDetails.text = viewMovies.name.value?.description
-        binding.imageMovieDetails.setImageResource(viewMovies.name.value?.image!!)
     }
 }
